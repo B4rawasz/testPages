1c0aff1ebe2fe6a1f9c65c069f1ce3f5ff0332bd
Barawasz <69699459+B4rawasz@users.noreply.github.com>
2025-03-04 18:53:49 +0100

    Remove unoptimized setting from Next.js image configuration

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -6,7 +6,6 @@ const nextConfig: NextConfig = {
 	output: "export", // <=== enables static exports
 	reactStrictMode: true,
 	images: {
-		unoptimized: true,
 		loader: "custom",
 		loaderFile: "./custom_img.js",
 	},
